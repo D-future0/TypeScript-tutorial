a0205a6d8264a8fb8e6c0d9fbf2bd0b44ec7a5c0
D-future0 <oka4collins@gmail.com>
2024-11-22 15:37:54 +0100

    fetching data from api

--- typescript/src/tutorial.ts
+++ typescript/src/tutorial.ts
@@ -371,4 +371,33 @@
 //  printValue(person)
 
 const url = `https://www.course-api.com/react-tours-project`
+type Tour = {
+    id:string;
+    name:string;
+    info:string;
+    image:string;
+    price:string;
+}
+
+async function fetchData(url:string): Promise<Tour[]>{
+    try {
+        const response = await fetch(url)
+        if(!response.ok){
+            throw new Error(`HTTP error! status : ${response.status}`)
+        }
+        const data:Tour[] = await response.json()
+
+        return data
+    } catch (error) {
+        const errMessage = error instanceof Error ? error.message : `there was an error`;
+        console.log(errMessage)
+        return []
+    }
+}
+
+const tour = await fetchData(url)
+tour.map((tour)=>{
+    // return tour.name
+    console.log(tour.name)
+})
 
